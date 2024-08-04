5a321197900984a305adba0cefda02e81c5b4d52
Владислав Кошелев <testvlad59@gmail.com>
2024-08-04 13:30:20 +0300

    внесены изменения по flake8 и mypy. но ошибки по прежнему есть

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,11 @@
-def get_mask_card_number(x):
+def get_mask_card_number(x: str) -> str:
     """Функцию маскировки номера банковской карты"""
     x = input()
     hidden_number = x[:6] + "******" + x[12:]
     return hidden_number
 
 
-def get_mask_account(x):
+def get_mask_account(x: str) -> str:
     """Функцию маскировки номера банковского счета"""
     x = input()
     hidden_number = "****" + x[-4:]
