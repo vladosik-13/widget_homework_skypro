d4cc3c9ee70d7b77ebcf317e92a93d4d21d8d69c
Владислав Кошелев <testvlad59@gmail.com>
2024-08-05 18:45:03 +0300

    Исправлены косяки оформления по PEP8 с помощью flake8

--- src/processing.py
+++ src/processing.py
@@ -4,14 +4,13 @@ def filter_by_state(status_operation, state='EXECUTED'):
     return filtered_list
 
 
-# Ниже раскомментируйте код для проверки
-#status_operation = [
+# раскомментируйте код ниже для проверки функции
+# status_operation = [
 #    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 #    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
 #    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
 #    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-#]
+# ]
 #
-#filtered_list = filter_by_state(status_operation)
-#print(filtered_list)
-
+# filtered_list = filter_by_state(status_operation)
+# print(filtered_list)
