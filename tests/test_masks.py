3c4015bfa2094388b0fb95e81e98335336bc9028
Владислав Кошелев <testvlad59@gmail.com>
2024-08-09 03:42:39 +0300

    создан файл с фикстурами ко всем функциям и тестам

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,14 +1,9 @@
-from src.masks import get_mask_card_number
+from src.masks import get_mask_card_number, get_mask_account
 
 
-
-def test_get_mask_card_number():
-    assert get_mask_card_number('7000792289606361') == '700079******6361'
-
-
-from src.masks import get_mask_account
+def test_get_mask_card_number(card_number):
+    assert get_mask_card_number('7000792289606361') == card_number
 
 
-
-def test_get_mask_account():
-    assert get_mask_account('73654108430135874305') == '**4305'
+def test_get_mask_account(mask_account):
+    assert get_mask_account('73654108430135874305') == mask_account
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,15 +1,13 @@
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_date
 
 
-def test_mask_account_card():
-    assert mask_account_card('Visa Platinum 7000792289606361') == 'VisaPlatinum **6361'
+def test_mask_account_card(card):
+    assert mask_account_card('Visa Platinum 7000792289606361') == card
 
 
-def test_2_mask_account_card():
-    assert mask_account_card('Счет 73654108430135874305') == 'Счет **4305'
-
-from src.widget import get_date
+def test_2_mask_account_card(two_card):
+    assert mask_account_card('Счет 73654108430135874305') == two_card
 
 
-def test_get_date():
-    assert get_date('2024-03-11T02:26:18.671407') == '11.03.2024'
+def test_get_date(date):
+    assert get_date('2024-03-11T02:26:18.671407') == date
