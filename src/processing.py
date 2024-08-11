dfb7cbb7ce181443c886bcd70a91738931317eb3
Владислав Кошелев <testvlad59@gmail.com>
2024-08-11 11:19:10 +0300

    исправлены ошибки по flake8

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def filter_by_state(status_operation, state="EXECUTED"):
     """Функция фильтрвции по ключу state (по умолчанию ключ = 'EXECUTED')"""
     filtered_list = [operation for operation in status_operation if operation.get("state", "EXECUTED") == state]
@@ -14,8 +17,6 @@ def filter_by_state(status_operation, state="EXECUTED"):
 #
 # filtered_list = filter_by_state(status_operation)
 # print(filtered_list)
-
-from datetime import datetime
 
 
 def sort_list_by_date(sorted_list, reverse=True):
--- src/widget.py
+++ src/widget.py
@@ -18,9 +18,6 @@ def mask_account_card(user_input: str) -> str:
     return f"{name_account} {account_to_mask}"
 
 
-
-
-
 def get_date(date_str: str) -> str:
     """Функция принимающая дату в формате "2024-03-11T02:26:18.671407" и возвращающая дату в формате "11.03.2024" """
     date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
