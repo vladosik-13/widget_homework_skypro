326695044dd7b4c8bf9cb97ee47b48a40534bdcf
Владислав Кошелев <testvlad59@gmail.com>
2024-08-18 14:35:29 +0300

    написан код декоратора log.py с горем пополам. по умолчанию возращает результат работы декоратора в консоль

--- src/decorators.py
+++ src/decorators.py
@@ -1,30 +1,47 @@
 import logging
-import sys
+
+logging.basicConfig(level=logging.INFO)
+
 
 def log(filename=None):
-    def decorator(func):
+    def decorator(function):
         def wrapper(*args, **kwargs):
-            if filename:
-                logging.basicConfig(filename=filename, level=logging.INFO)
-            else:
-                logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
+            log_message = (
+                f"Запуск функции: {function.__name__} ok\n"
+                f"Функция {function.__name__} выполнена.\n"
+            )
             try:
-                result = func(*args, **kwargs)
-                logging.info(f'{func.__name__} ok')
-                return result
+                result = function(*args, **kwargs)
+
+                if filename:
+                    with open(filename, 'w', encoding='utf-8') as file:
+                        file.write(log_message + "-" * 40 + "\n")
+                else:
+                    print(log_message)
+
+                return result  # возврат результата выполнения функции
+
             except Exception as e:
-                logging.error(f'{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}')
-                raise e
+                error_message = f"{function.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}\n"
+
+                if filename:
+                    with open(filename, "w", encoding='utf-8') as file:
+                        file.write(error_message)
+                else:
+                    print(error_message)
+
+                return None  # Возвращаем None, чтобы не зацикливать вызов wrapper
 
         return wrapper
+
     return decorator
 
 
+@log('')
+def result(x, y):
+    nums = x / y
+    return nums
 
 
-@log(filename=None)
-def summa(x, y):
-    return x + y
-z = summa(35, 4)
-print(z)
+# Вызов функции для теста
+result(10, 2)
