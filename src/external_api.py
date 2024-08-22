f274d43c6c49bd8b3f676eee4dcb29960f345de7
Владислав Кошелев <testvlad59@gmail.com>
2024-08-23 02:53:22 +0300

    реализована функция которая принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float. Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения текущего курса валют и конвертации суммы операции в рубли

---
+++ src/external_api.py
@@ -0,0 +1,66 @@
+import requests
+import json
+
+def get_transaction_amount(transaction):
+    """принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float
+    . Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения текущего курса валют и
+     конвертации суммы операции в рубли. Для конвертации валюты воспользуйтесь Exchange Rates Data API"""
+    if transaction["operationAmount"]["currency"]["code"] == "RUB":
+        return float(transaction["operationAmount"]["amount"])
+    elif transaction["operationAmount"]["currency"]["code"] == "EUR":
+            pay = transaction["operationAmount"]["amount"]
+            url = f'https://api.apilayer.com/exchangerates_data/convert?to=rub&from=eur&amount={pay}'
+
+            payload = {}
+            headers = {
+                "apikey": "uFbHNFlqfpRJeUW7yRneNUoUmZUQ8vXl"
+            }
+
+            response = requests.request("GET", url, headers=headers, data=payload)
+            pay = transaction["operationAmount"]["amount"]
+
+            status_code = response.status_code
+            result = response.text
+            data_dict = json.loads(result)
+            convert_pay = data_dict['result']
+            return convert_pay
+    elif transaction["operationAmount"]["currency"]["code"] == "USD":
+            pay = transaction["operationAmount"]["amount"]
+            url = f'https://api.apilayer.com/exchangerates_data/convert?to=rub&from=eur&amount={pay}'
+
+            payload = {}
+            headers = {
+                "apikey": "uFbHNFlqfpRJeUW7yRneNUoUmZUQ8vXl"
+            }
+
+            response = requests.request("GET", url, headers=headers, data=payload)
+            pay = transaction["operationAmount"]["amount"]
+
+            status_code = response.status_code
+            result = response.text
+            data_dict = json.loads(result)
+            convert_pay = data_dict['result']
+            return convert_pay
+
+"""Раскоментируйте код для проверки функции
+test = get_transaction_amount({
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  })
+print(test)
+"""
+
+
+if __name__ == '__external_api__':
+    external_api()
