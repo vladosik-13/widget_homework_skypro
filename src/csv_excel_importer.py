1202947cbbb723d6c84f0d33eb8d8ce6fff32466
Владислав Кошелев <testvlad59@gmail.com>
2024-08-28 20:15:22 +0300

    Реализована функция csv_import которая принимает аргументом путь к файлу .csv и возвращает список словарей с транзакциями

---
+++ src/csv_excel_importer.py
@@ -0,0 +1,14 @@
+import csv
+import pandas
+
+def csv_import(path_file):
+    """Функция принимает аргументом путь к файлу .csv и возвращает список словарей с транзакциями"""
+    with open(path_file, 'r',encoding='utf-8') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)    # закоментируй строку чтобы выодилась первая строка тоже
+        for row in reader:
+            print(row)
+
+# раскоментируй код ниже чтобы проверить работу функции
+"""test_func = csv_import('C:/Users/user/PycharmProjects/home_work_widget/data/transactions.csv')
+print(test_func)"""
