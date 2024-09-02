522e27427ab8e06b4079f7c6c5ca9ff4d45ecaf5
Владислав Кошелев <testvlad59@gmail.com>
2024-09-03 00:24:11 +0300

    реализована функция принимает список словарей с данными о банковских операциях     и список категорий операций, возвращает словарь с количественными данными по категориям

--- src/search_list_dict.py
+++ src/search_list_dict.py
@@ -28,4 +28,40 @@ def search_transactions(transactions, search_string):
 
 search_string = 'Перевод со счета на счет'
 filtered_transactions = search_transactions(transactions, search_string)
-print(filtered_transactions)'''
+print(filtered_transactions)
+'''
+
+
+def categorize_transactions(transactions, categories):
+    """Функция принимает список словарей с данными о банковских операциях
+    и список категорий операций, возвращает словарь с количественными данными по категориям."""
+    category_count = {category: 0 for category in categories}
+    for transaction in transactions:
+        description = transaction.get('description', '')
+        for category in categories:
+            if category in description:
+                category_count[category] += 1
+                break  # Если нашли совпадение, переходим к следующей транзакции
+    return category_count
+
+
+# раскоментируйте код ниже чтобы проверить ф-ю
+'''transactions = [
+    {'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041',
+     'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}},
+     'description': 'Перевод организации', 'from': 'Maestro 1596837868705199',
+     'to': 'Счет 64686473678894779589'},
+    {'id': 587085106, 'state': 'EXECUTED', 'date': '2018-03-23T10:45:06.972075',
+     'operationAmount': {'amount': '48223.05', 'currency': {'name': 'руб.', 'code': 'RUB'}},
+     'description': 'Открытие вклада', 'to': 'Счет 41421565395219882431'},
+    {'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878',
+     'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}},
+     'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542',
+     'to': 'Счет 75651667383060284188'}
+]
+
+# Задаем категории для подсчета
+categories = ['Перевод', 'Открытие', 'Оплата']
+# Получаем количество операций по категориям
+result = categorize_transactions(transactions, categories)
+print(result)'''
